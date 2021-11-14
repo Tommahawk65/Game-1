7431f2598a3a9d3769fc497b7b121a10bfa6cfb4
Tommahawk65 <74880176+Tommahawk65@users.noreply.github.com>
2021-11-14 20:52:18 +0000

    Game Logic Working

--- assets/game.js
+++ assets/game.js
@@ -51,12 +51,18 @@ document.addEventListener('DOMContentLoaded', () => {
         },
     ]
     
-    
+    let flippedCard = false;
+    let firstCard, secondCard;
+    let gameLock = false;
+    let pairs = 6;
     const card = document.querySelectorAll('.card');
+    let cardsChosen = []
+    let cardsChosenId = []
     document.getElementById("reset-btn").addEventListener('click', reset);
     document.getElementById("replay-btn").addEventListener('click', reset);
     cardArray.sort(() => 0.5 - Math.random())
     const grid = document.getElementById('game-content')
+    const done = document.getElementById('done-msg');
     
     function board() {
         for (let i = 0; i < cardArray.length; i++) {
@@ -64,11 +70,85 @@ document.addEventListener('DOMContentLoaded', () => {
             gamecard.className = 'card';
             gamecard.setAttribute('data-id', i)
             gamecard.innerHTML = '<img class="back-face" src="assets/images/pokeball.png" alt="back card">'
+            gamecard.addEventListener('click', cardFlip);
             grid.appendChild(gamecard)
             
         }
     }
     
     board()
+
+    function cardFlip() {
+        this.className = ('card flip')
+        let cardId = this.getAttribute('data-id')
+        var front = document.createElement('img')
+        front.className = "front-face"
+        front.src = cardArray[cardId].img;
+        front.alt = "front card" 
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenId.push(cardId)
+    
+        if (gameLock) return;
+        if (this === firstCard) return;
+        this.appendChild(front)
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+       
+        if (!flippedCard) {
+            flippedCard = true;
+            firstCard = this;
+        } else {
+            flippedCard = false;
+            secondCard = this;
+            if (cardsChosen[0] === cardsChosen[1]) {
+                
+                match ()
+    
+            } else { 
+                // Not a match
+                flipBack ()    
+            }
+            cardsChosen = []
+            cardsChosenId = []
+        }
+
+        // Check For Match 
+        function match() {
+            setTimeout(() => {
+            firstCard.removeEventListener('click', cardFlip);
+            firstCard.classList.add('green-light');
+            secondCard.removeEventListener('click', cardFlip);
+            secondCard.classList.add('green-light');
+            }, 1200); 
+            pairs --;
+        }
+        
+        // Cards Dont Match
+        function flipBack() {
+            setTimeout(() => {
+            gameLock = true;
+            firstCard.classList.add('red-light');
+            secondCard.classList.add('red-light');
+            }, 1200);
+        
+            setTimeout(() => {
+                firstCard.classList.remove('flip');
+                firstCard.classList.remove('red-light');
+                secondCard.classList.remove('flip');
+                secondCard.classList.remove('red-light');
+                gameLock = false;
+            }, 2600);
+        }
+ 
+ 
+    
+
+    // Win Message
+    if (pairs <= 0) {
+        setTimeout(() => {
+        done.style.display = 'block';
+       }, 1000);   
+      }
+    }
 
 })
